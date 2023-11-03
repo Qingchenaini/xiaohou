c09af4f15133bdd16858e1fa7ea878f76510ae9b
Qingchenaini <2822948620@qq.com>
2023-11-03 10:21:35 +0800

    升级react18

--- src/index.tsx
+++ src/index.tsx
@@ -1,41 +1,41 @@
 import React, { useEffect } from 'react';
 import ReactDOM from 'react-dom';
-import { useRequest } from 'ahooks';
-import { getDate } from './api/date';
-import fetchGithubStars from './api/star';
-import { getBookByParams, getBookByQuery } from './api/book';
+import { createRoot } from 'react-dom/client';
 import './index.scss';
 import Header from './web/layout/header';
 import Footer from './web/layout/footer';
 import { Routes, Route } from "react-router-dom";
 import { BrowserRouter as Router } from 'react-router-dom';
 import SiderCard from './web/layout/siderCard';
-import Blog from './web/components/blog';
 import ArticleAll from './web/components/articleall';
 import ArticleOne from './web/components/articleOne';
 import { FloatButton } from 'antd';
 import { UpCircleTwoTone } from '@ant-design/icons'
-
+import { ThemeProvider } from 'antd-style';
 
 
 function App() {
   return (
     <Router>
-      <div className="app" >
-        <Header />
-        <div className="middle">
-          <SiderCard />
-          <Routes>
-            {/* <Route path='/Blog' element={<Blog />} /> */}
-            <Route path='/' element={<ArticleAll />} />
-            <Route path='/article' element={<ArticleOne />} />
-          </Routes>
+      <ThemeProvider appearance={'dark'}>
+        <div className="app" >
+          <Header />
+          <div className="middle">
+            <SiderCard />
+            <Routes>
+              {/* <Route path='/Blog' element={<Blog />} /> */}
+              <Route path='/' element={<ArticleAll />} />
+              <Route path='/article' element={<ArticleOne />} />
+            </Routes>
+          </div>
+          <Footer />
+          <FloatButton.BackTop icon={<UpCircleTwoTone />} />
         </div>
-        <Footer />
-        <FloatButton.BackTop icon={<UpCircleTwoTone />} />
-      </div>
+      </ThemeProvider>
     </Router>
   );
 }
-
-ReactDOM.render(<App />, document.getElementById('root'));
+const container = document.getElementById('root');
+const root = createRoot(container); // TypeScript 使用 createRoot(container!)
+root.render(<App />);
+// ReactDOM.render(<App />, document.getElementById('root'));
